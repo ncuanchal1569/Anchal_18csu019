7fa9e6f6dfaf361955b0d6656f4ba22f50fcd5df
Anchal Mishra <anchal18csu019@ncuindia.edu>
2019-02-04 21:04:18 +0530

    Add files

---
+++ Assignment 2/class/Address.java
@@ -0,0 +1,17 @@
+package com.ncu.assignment.classes;
+class Address
+{
+	String line1;
+	String line2;
+	String city;
+	String state;
+	int pinCode;
+	Address(String line1, String line2, String city, String state, int pinCode)
+	{
+	  this.line1= line1;
+	  this.line2= line2;
+	  this.city= city;
+	  this.state= state;
+	  this.pinCode= pinCode;
+	}
+}
---
+++ Assignment 2/class/Project.java
@@ -0,0 +1,17 @@
+package com.ncu.assignment.classes;
+class Project
+{
+	String name;
+	String startDate;
+	String endDate;
+	String role;
+	String[] responsibilities;
+	Project(String Name,String startDate, String endDate, String role, String responsibilities[])
+	{
+	  this.name= name;
+	  this.startDate= startDate;
+	  this.endDate= endDate;
+	  this.role= role;
+	  this.responsibilities= responsibilites;
+	}
+}
